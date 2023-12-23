3a622b42aeac7240a20863a8cda6b27437492045
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2023-12-23 06:28:28 +0530

    Added all four basic method of Http request

--- Express-Project/hospital.js
+++ Express-Project/hospital.js
@@ -1,19 +1,20 @@
 const express = require("express")
 const app = express();
 
-const user = [{
+let user = [{
 	name:"john",
 	kidneys:[{
-			healthy:false
+			healthy:true
 		}]
-}]
+}];
 
+app.use(express.json())
 app.get("/", (req,res)=>{
 	let johnkidneys = user[0]["kidneys"].length;
 	let healthykid = 0;
 	for(let i=0;i<johnkidneys;i++)
 	{
-		if(user[0]["kidneys"].healthy == true)
+		if(user[0]["kidneys"][i].healthy === true)
 		{
 			healthykid++;
 		}
@@ -25,7 +26,6 @@ app.get("/", (req,res)=>{
 		unhealthy
 	})
 })
-.use(express.json())
 .post("/",(req,res)=>{
 	const isHealthy = req.body.isHealthy
 	user[0].kidneys.push({healthy:isHealthy})
@@ -35,9 +35,16 @@ app.get("/", (req,res)=>{
 	let johnkidneys = user[0]["kidneys"].length;
         for(let i=0;i<johnkidneys;i++)
         {
-               user[0]["kidneys"].healthy = true
+               user[0]["kidneys"][i].healthy = true
         }
 	res.send("")	
+})
+.delete("/",(req,res)=>{
+	user[0]["kidneys"]  = user[0]["kidneys"].filter((ele)=>{
+		if(ele.healthy == true)
+			return ele;
+	})
+	res.send("")
 })
 .listen(3000,()=>{
 	console.log("App is listening at 3000")
