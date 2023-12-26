8436c8e7dc8232c452a3e45fa8096c8f01157521
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2023-12-27 05:09:27 +0530

    Zod for validation

---
+++ zod/validate1.js
@@ -0,0 +1,10 @@
+const zod = require("zod")
+
+function validate(arr)
+{
+	const schema = zod.array(zod.number())
+	const response = schema.safeParse(arr)
+	console.log(response)
+}
+
+validate([1,2,3,4])
